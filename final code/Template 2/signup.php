5f2197fbada563dd28b66a69be87fe2f007a66c4
AntonyDasgupta <ronosupujit123@gmail.com>
2025-09-03 01:36:30 +0530

    udpated code

--- final code/Template 2/signup.php
+++ final code/Template 2/signup.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->bind_param("sss", $fullname, $email, $hashedPassword);
 
     if ($stmt->execute()) {
-        echo "Signup successful. <a href='login.html'>Login here</a>";
+        echo "Signup successful. <a href='login.html'>Login antonyyyy    here</a>";
     } else {
         echo "Error: " . $conn->error;
     }
